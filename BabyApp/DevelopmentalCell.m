6e1f6960f6e3956069366e6b4900b116d3ece29d
iShadowX <sandipdave@Sandips-Mac-mini.local>
2016-04-24 20:25:05 +0530

    Arbaz
    Developmental screen

---
+++ BabyApp/DevelopmentalCell.m
@@ -0,0 +1,23 @@
+//
+//  DevelopmentalCell.m
+//  BabyApp
+//
+//  Created by Sandeep Dave on 24/04/16.
+//  Copyright © 2016 Infinity. All rights reserved.
+//
+
+#import "DevelopmentalCell.h"
+
+@implementation DevelopmentalCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ BabyApp/DevelopmetalScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  DevelopmetalScreenViewController.h
+//  BabyApp
+//
+//  Created by Sandeep Dave on 24/04/16.
+//  Copyright © 2016 Infinity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DevelopmetalScreenViewController : UIViewController
+
+@end
