2a1c80bb5e6a2ebc045bd5a1f46f7913c7cee8d6
atulawasthimarch <atul.iosdeveloper@gmail.com>
2016-04-25 11:46:53 +0530

    oral fxxcxxs

--- BabyApp/OralHealth.m
+++ BabyApp/OralHealth.m
@@ -27,19 +27,25 @@ UIButton *b1,*b2;
         _bhImage.image=[UIImage imageNamed:@"Oral health.png"];
         self.navigationItem.title = @"ORAL HEALTH";
         
-        b1=[[UIButton alloc] initWithFrame:CGRectMake(0, self.navigationController.navigationBar.frame.size.height, self.view.frame.size.width/2, 30)];
+        b1=[[UIButton alloc] initWithFrame:CGRectMake(0,0, self.view.frame.size.width/2, 40)];
         
-        b2=[[UIButton alloc] initWithFrame:CGRectMake(self.view.frame.size.width/2, self.navigationController.navigationBar.frame.size.height, self.view.frame.size.width/2, 30)];
+        b2=[[UIButton alloc] initWithFrame:CGRectMake(self.view.frame.size.width/2,0, self.view.frame.size.width/2, 40)];
         [self.view addSubview:b1];
         [self.view addSubview:b2];
         [b1 setTitle:@"ADULT TEETH" forState:UIControlStateNormal];
         [b2 setTitle:@"BABY TEETH" forState:UIControlStateNormal];
         
-        [b1 setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
-        [b2 setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [b1 setTitleColor:[UIColor colorWithRed:108.0/255.0 green:107.0/255.0 blue:108.0/255.0 alpha:1.0] forState:UIControlStateNormal];
+        [b2 setTitleColor:[UIColor colorWithRed:108.0/255.0 green:107.0/255.0 blue:108.0/255.0 alpha:1.0] forState:UIControlStateNormal];
         
         [b1 setBackgroundColor:[UIColor whiteColor]];
-        [b2 setBackgroundColor:[UIColor grayColor]];
+        [b2 setBackgroundColor:[UIColor colorWithRed:235.0/255.0 green:235.0/255.0 blue:240.0/255.0 alpha:1.0]];
+
+        [[b1 titleLabel] setFont:[UIFont fontWithName:@"AvenirNextLTPro-Demi" size:14]];
+        [[b2 titleLabel] setFont:[UIFont fontWithName:@"AvenirNextLTPro-Demi" size:14]];
+
+        [b1 addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+        [b2 addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
 
 
     }
@@ -56,7 +62,6 @@ UIButton *b1,*b2;
     [_bhImage setContentMode:UIViewContentModeScaleAspectFit];
     [_bhImage setClipsToBounds:YES];
     
-    
     // Do any additional setup after loading the view.
 }
 -(void)btnClick:(UIButton*)b
@@ -64,14 +69,17 @@ UIButton *b1,*b2;
     if(b==b1)
     {
         [b1 setBackgroundColor:[UIColor whiteColor]];
-        [b2 setBackgroundColor:[UIColor grayColor]];
+        [b2 setBackgroundColor:[UIColor colorWithRed:235.0/255.0 green:235.0/255.0 blue:240.0/255.0 alpha:1.0]];
+        _bhImage.image=[UIImage imageNamed:@"Oral health.png"];
 
     }
     else
     {
         [b2 setBackgroundColor:[UIColor whiteColor]];
-        [b1 setBackgroundColor:[UIColor grayColor]];
+        [b1 setBackgroundColor:[UIColor colorWithRed:235.0/255.0 green:235.0/255.0 blue:240.0/255.0 alpha:1.0]];
+        _bhImage.image=[UIImage imageNamed:@"babyTeeth.png"];
 
+        
     }
 }
 - (void)didReceiveMemoryWarning {
